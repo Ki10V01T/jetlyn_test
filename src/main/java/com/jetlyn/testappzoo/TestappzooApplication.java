05e0f7cb24494f87abbf378334b1ad43fb1367cc
Ki10V01T <ampernaom@gmail.com>
2023-04-06 21:09:22 +0300

    initial commit spring boot

---
+++ src/main/java/com/jetlyn/testappzoo/TestappzooApplication.java
@@ -0,0 +1,13 @@
+package com.jetlyn.testappzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestappzooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestappzooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jetlyn/testappzoo/TestappzooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetlyn.testappzoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestappzooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
